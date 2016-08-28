d3a2b2e9162ab62ba5fd8e5a4efd8f10ed441d9b
tannerkrewson <tannerkrewson@gmail.com>
2016-08-28 18:40:37 -0500

    Added buttons

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,29 @@ var Spyout = React.createClass({
     return (
 			<div className="main-content text-xs-center" id="spyout">
       	<p id="title">SPYOUT</p>
+        <hr/>
+        <MainMenu />
 				<Bottom />
 			</div>
+    );
+  }
+});
+
+var MainMenu = React.createClass({
+  render: function() {
+    return (
+      <div className="main-menu">
+        <SOButton label="Join Game" />
+        <SOButton label="New Game" />
+      </div>
+    );
+  }
+});
+
+var SOButton = React.createClass({
+  render: function() {
+    return (
+      <button type="button" className="btn btn-secondary sobutton">{this.props.label}</button>
     );
   }
 });
