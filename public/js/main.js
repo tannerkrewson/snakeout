a7efaac1d542cb9d4836006aa4318265617bd058
tannerkrewson <tannerkrewson@gmail.com>
2016-08-28 00:32:51 -0500

    Designed home page

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,28 @@
+var Spyout = React.createClass({
+  render: function() {
+    return (
+			<div className="main-content text-xs-center" id="spyout">
+      	<p id="title">SPYOUT</p>
+				<Bottom />
+			</div>
+    );
+  }
+});
+
+var Bottom = React.createClass({
+	render: function() {
+		return (
+			<footer>
+				<hr/>
+				Spyout by Tanner Krewson
+				<br/>
+				<a href="http://www.tannerkrewson.com/" target="_blank">www.tannerkrewson.com</a>
+			</footer>
+		);
+	}
+});
+
 ReactDOM.render(
-  <h1>Spyout</h1>,
+  <Spyout />,
   document.getElementById('root')
 );
