1d60be307a4642dfacf44d2de0cdd2c6495815a7
tannerkrewson <tannerkrewson@gmail.com>
2016-08-30 17:16:01 -0500

    Added Join and New Game menus

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,16 @@
 var Spyout = React.createClass({
+  getInitialState: function() {
+    return {page: MainMenu};
+  },
+  changePage: function(page) {
+    this.setState({page});
+  },
   render: function() {
     return (
 			<div className="main-content text-xs-center" id="spyout">
       	<p id="title">SPYOUT</p>
         <hr/>
-        <MainMenu />
+        <this.state.page changePage={this.changePage} />
 				<Bottom />
 			</div>
     );
@@ -13,19 +19,73 @@ var Spyout = React.createClass({
 
 var MainMenu = React.createClass({
   render: function() {
+    var goToJoinGame = function() {
+      this.props.changePage(JoinGame);
+    };
+    var goToNewGame = function() {
+      this.props.changePage(NewGame);
+    };
     return (
       <div className="main-menu">
-        <SOButton label="Join Game" />
-        <SOButton label="New Game" />
+        <SOButton label="Join Game" onClick={goToJoinGame.bind(this)}/>
+        <SOButton label="New Game"  onClick={goToNewGame.bind(this)}/>
+      </div>
+    );
+  }
+});
+
+var JoinGame = React.createClass({
+  render: function() {
+    var goToMainMenu = function() {
+      this.props.changePage(MainMenu);
+    };
+    return (
+      <div className="join-menu">
+        <p>Enter the game code:</p>
+        <SOInput placeholder="" />
+        <br/><br/>
+        <p>Enter your name:</p>
+        <SOInput placeholder="" />
+        <br/><br/>
+        <SOButton label="Back" onClick={goToMainMenu.bind(this)}/>
+        <SOButton label="Join" />
+      </div>
+    );
+  }
+});
+
+var NewGame = React.createClass({
+  render: function() {
+    var goToMainMenu = function() {
+      this.props.changePage(MainMenu);
+    };
+    return (
+      <div className="new-menu">
+        <p>Enter your name:</p>
+        <SOInput placeholder="" />
+        <br/><br/>
+        <SOButton label="Back" onClick={goToMainMenu.bind(this)}/>
+        <SOButton label="Start" />
       </div>
     );
   }
 });
 
 var SOButton = React.createClass({
+  getInitialState: function() {
+    return {disabled: false};
+  },
   render: function() {
     return (
-      <button type="button" className="btn btn-secondary sobutton">{this.props.label}</button>
+      <button type="button" className="btn btn-secondary sobutton" disabled={this.state.disabled} onClick={this.props.onClick}>{this.props.label}</button>
+    );
+  }
+});
+
+var SOInput = React.createClass({
+  render: function() {
+    return (
+      <input type="text" placeholder={this.props.placeholder} />
     );
   }
 });
@@ -34,7 +94,7 @@ var Bottom = React.createClass({
 	render: function() {
 		return (
 			<footer>
-				<hr/>
+        <hr/>
 				Spyout by Tanner Krewson
 				<br/>
 				<a href="http://www.tannerkrewson.com/" target="_blank">www.tannerkrewson.com</a>
