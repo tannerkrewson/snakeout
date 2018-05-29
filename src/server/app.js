94541b6587d7b024da578a733a257a446b955643
tannerkrewson <tannerkrewson@gmail.com>
2018-05-28 23:02:25 -0700

    Cleaned up server code

--- src/server/app.js
+++ src/server/app.js
@@ -1,32 +1,23 @@
 var express = require('express');
 var socketio = require('socket.io');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 var io = socketio();
 app.io = io;
 
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 var devModeEnabled = (app.get('env') === 'development');
 
+// startup the server-side code
 var Spyout = require('./app/spyout');
 app.spyout = new Spyout(devModeEnabled);
-
 require('./routes/socketio')(app);
 
 // serve the compiled client code
 app.use(express.static('dist'));
-
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, '../../public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-	extended: false
-}));
-app.use(cookieParser());
 
 module.exports = app;
