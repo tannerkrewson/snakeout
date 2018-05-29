a91ac442d3689ae06fb279dfdea1d5d9f53b652b
tannerkrewson <tannerkrewson@gmail.com>
2018-05-28 22:39:57 -0700

    Made it work in production mode

--- src/server/app.js
+++ src/server/app.js
@@ -15,52 +15,18 @@ var devModeEnabled = (app.get('env') === 'development');
 var Spyout = require('./app/spyout');
 app.spyout = new Spyout(devModeEnabled);
 
-require('./routes')(app);
+require('./routes/socketio')(app);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// serve the compiled client code
+app.use(express.static('dist'));
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, '../../public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
 	extended: false
 }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	var err = new Error('Not Found');
-	err.status = 404;
-	next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-	app.use(function (err, req, res, next) {
-		res.status(err.status || 500);
-		res.render('error', {
-			message: err.message,
-			error: err
-		});
-	});
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-	res.status(err.status || 500);
-	res.render('error', {
-		message: err.message,
-		error: {}
-	});
-});
-
 
 module.exports = app;
