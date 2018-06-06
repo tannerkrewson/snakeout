97abff3ed22a297690968c90adc9351fec249099
tannerkrewson <tannerkrewson@gmail.com>
2018-06-06 14:58:35 -0700

    Show disconnected players on waiting screen

--- src/client/components/Waiting.js
+++ src/client/components/Waiting.js
@@ -6,12 +6,24 @@ import RoundInfoBar from "./RoundInfoBar";
 export default class Waiting extends Component {
 	render() {
 		var me = this.props.you;
-		var data = this.props.round;
+		var round = this.props.round;
+
+		const disconnected = round.disconnectedList.length > 0;
 		return (
 			<div className="waiting">
 				<p>{this.props.message}</p>
-				<PlayerList players={this.props.round.waitingList} />
-				<RoundInfoBar round={data} me={me} />
+				<PlayerList players={round.waitingList} />
+				{disconnected && (
+					<div>
+						<p>Disconnected Players: </p>
+						<PlayerList players={round.disconnectedList} />
+						<p>
+							The game will continue when someone joins this game to take their
+							place.
+						</p>
+					</div>
+				)}
+				<RoundInfoBar round={round} me={me} />
 			</div>
 		);
 	}
