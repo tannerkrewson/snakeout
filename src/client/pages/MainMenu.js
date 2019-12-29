de67bef2d6b4a9ab2507dc6183ece7a7f06ef054
tannerkrewson <tannerkrewson@gmail.com>
2019-12-28 20:16:38 -0600

    add apology alert

--- src/client/pages/MainMenu.js
+++ src/client/pages/MainMenu.js
@@ -45,6 +45,18 @@ export default class MainMenu extends Component {
 
 		return (
 			<div className="main-menu noformrefresh">
+				<div class="alert alert-dark soalert" role="alert">
+					I've fixed the bug that caused missions to always fail. My apologies
+					to anyone who has played before! The game should work now.
+					<br />
+					<br />
+					To make up for this, I've updated Spyout with a new design and other
+					fixes. :D
+					<br />
+					<br />
+					Please email me at tannerkrewson@gmail.com if you find any other
+					issues in the future. Thanks!
+				</div>
 				<p>
 					<SOButton onClick={goToJoinGame.bind(this)}>Join Game</SOButton>
 					<SOButton onClick={goToNewGame.bind(this)}>New Game</SOButton>
